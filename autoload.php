98fa9c7dc520ebc08c75ffdc1a9da09298d98db3
igor barbosa de carvalho <igor@localhost.localdomain>
2017-10-08 15:25:57 -0300

    implementando auloado de classes

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+    function customAutoload($className) {
+        
+        if (file_exists($className . '.php')){
+            require_once $className . '.php';
+        }
+    }
+
+    spl_autoload_register('customAutoload');
